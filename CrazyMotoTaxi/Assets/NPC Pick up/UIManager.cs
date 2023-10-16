2070afd1682acc6a0107da50ee24f959df73453c
Julian Gaitan <58521037+GaitanJulian@users.noreply.github.com>
2023-10-15 21:31:01 -0500

    fixed HUD

--- CrazyMotoTaxi/Assets/NPC Pick up/UIManager.cs
+++ CrazyMotoTaxi/Assets/NPC Pick up/UIManager.cs
@@ -7,7 +7,6 @@ public class UIManager : MonoBehaviour
 {
     public TextMeshProUGUI timeLefText;
     public TextMeshProUGUI scoreText;
-    public TextMeshProUGUI currentFareText;
 
     private void OnEnable()
     {
@@ -15,20 +14,14 @@ public class UIManager : MonoBehaviour
         {
             GameManager.Instance.OnScoreUpdated += UpdateScore;
             GameManager.Instance.OnTimeLeftUpdated += UpdateTimeLeft;
-            GameManager.Instance.OnActualFareUpdated += UpdateCurrentFare;
         }
-        else
-        {
-            print("lol");
-        }
-   
     }
 
     private void UpdateScore(float score)
     {
         // Round the score to the nearest 50 units
         int roundedScore = Mathf.RoundToInt(score / 50) * 50;
-        scoreText.text = "Cash: " + roundedScore + "$";
+        scoreText.text = roundedScore + " $";
     }
 
     private void UpdateTimeLeft(float timeLeft)
@@ -36,12 +29,5 @@ public class UIManager : MonoBehaviour
         // Format the time without decimals
         int timeWithoutDecimals = Mathf.FloorToInt(timeLeft);
         timeLefText.text = "Time left: " + timeWithoutDecimals.ToString();
-    }
-
-    private void UpdateCurrentFare(float currentFare)
-    {
-        // Round the currentFare to the nearest 50 units
-        int roundedFare = Mathf.RoundToInt(currentFare / 50) * 50;
-        currentFareText.text = roundedFare + "$";
     }
 }
